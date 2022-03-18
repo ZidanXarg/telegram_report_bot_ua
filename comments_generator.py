0549b7ffcb7463d1d786425aad2788daf5e806e4
Vitaly Nikolaiev <vitaly.nikolaiev@gmail.com>
2022-03-18 12:00:20 +0200

    Add encoding for proper parsing.

--- comments_generator.py
+++ comments_generator.py
@@ -4,7 +4,7 @@ import random
 
 class CommentsGenerator:
     def __init__(self, utterances_path="comments/text_comments.txt"):
-        with open(utterances_path, 'r') as fp:
+        with open(utterances_path, 'r', encoding='utf8') as fp:
             self._utterances = [x.strip() for x in fp.readlines()]
 
     def get_random_text_comment(self):
